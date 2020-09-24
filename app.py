7f19aae17b40ad4274bfbaea4dd823327a7fcb94
udhbhav thandra <udhbhavthandra123@gmail.com>
2020-09-24 22:22:50 +0530

    first commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__,)
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("data/data1.yml")
- 
+lst=[]
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -16,7 +16,17 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    #print(userText)
+    #if len(lst)!=0:
+    #    print(lst[-1])
+    #if(len(lst)!=0 and "phone" in lst[-1]):
+    #    print("first if")
+    #   if(userText.lower()=="okay"):
+    #       return "Please enter your details"
+    #   else:
+    #       return "please enter okay"
+    #lst.append(str(english_bot.get_response(userText)))
+    return str(english_bot.get_response(userText.lower()))
  
  
 if __name__ == "__main__":
